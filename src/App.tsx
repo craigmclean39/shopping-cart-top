c4f594a3c88897072ac850d97736d65f1295d1a9
Craig McLean <craig.mclean39@gmail.com>
2021-10-08 15:24:56 -0700

    Add product.json and test loading it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface HeroImage {
+  color: string;
+  image: string;
+}
+
+interface Product {
+  category: string;
+  name: string;
+  description: string[];
+  colors: string[];
+  heroImages: HeroImage[];
+  detailImages: string[];
+  price: number;
+  sizes: string[];
+  activities: string[];
+}
 
 function App() {
-  return <div className='App'>TESTING</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('products.json', {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+
+      setIsLoading(false);
+      const products: Product[] = [];
+      for (let i = 0; i < data.length; i++) {
+        products.push(data[i]);
+      }
+    }
+
+    getData();
+  }, []);
+
+  return isLoading ? <div>Loading</div> : <div>Loaded</div>;
 }
 
 export default App;
