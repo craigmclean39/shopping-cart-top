7a83d2ddfc147f84ecebe3cdb2faeb172270ba89
Craig McLean <craig.mclean39@gmail.com>
2021-10-16 21:14:20 -0700

    Add Logo to home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HeroImage from '../images/hero2.png';
+import { ReactComponent as Logo } from '../images/icons/logo.svg';
 
 const Home = () => {
   return (
     <React.Fragment>
       <div className='main-page'>
+        <Logo fill='white' className='logo-img' />
         <div className='main-img-link-grid'>
           <Link className='shop-link' to='/shop'>
             SHOP
